4930cdefef954be81263bab2792934445661f85d
AlexxNB <iam@alexxnb.ru>
2021-02-06 11:13:54 +0300

    improve dev mode

--- docker/dev.sh
+++ docker/dev.sh
@@ -3,5 +3,8 @@
 #Remove running container if exists
 docker rm --force Doconv || true
 
+# Run package dev mode
+npm run pkg:dev &
+
 #Create new container with mounted project directory
 docker run --name Doconv -p 3000:3000 -v $(pwd):/app doconv:latest npm run dev
